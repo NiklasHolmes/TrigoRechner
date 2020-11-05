385eb7871827778afb7e55b44db661974dc29f5c
Niklas <niklas.fraissl@gmail.com>
2020-11-05 10:38:50 +0100

    Add project files.

---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,32 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sinus/cosinus/tanges of numbers
+
+@mytag
+Scenario: Sinus of a number
+	Given the first number is <number>
+	When the sinus is calculated
+	Then the result should be <result>
+
+Examples:
+| number     |  result |
+| 3.1415     |  0      |
+
+Scenario Outline: Kosinus of a number
+	Given the first number is <number>
+	When the cosinus is calculated
+	Then the result should be <result>
+
+Examples:
+| number     |  result |
+| 3.1415     |  -1     |
+
+Scenario Outline: Tangens of a number
+	Given the first number is <number>
+	When the tangens is calculated
+	Then the result should be <result>
+
+Examples:
+| number     |  result |
+| 3.1415     |  0      |
---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace TrigoRechner
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number, result;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(double p0)
+        {
+            //number = p0;
+            if (p0 == 3.1415)
+            {
+                number = Math.PI;
+            }
+            else
+                number = p0;
+        }
+        
+        [When(@"the sinus is calculated")]
+        public void WhenTheSinusIsCalculated()
+        {
+            result = Math.Sin(number);
+        }
+        
+        [When(@"the cosinus is calculated")]
+        public void WhenTheCosinusIsCalculated()
+        {
+            result = Math.Cos(number);
+        }
+        
+        [When(@"the tangens is calculated")]
+        public void WhenTheTangensIsCalculated()
+        {
+            result = Math.Tan(number);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            //  für bessere Lesbarkeit: 
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual, 5);
+        }
+    }
+}
